53116e3c2f2be02214a787021517cc0e20582d73
Daniel Foster <danieljfoster87@gmail.com>
2018-05-21 19:16:51 -0400

    fixed page title alignment

--- public/assets/js/bootcamp.js
+++ public/assets/js/bootcamp.js
@@ -1,8 +1,10 @@
 $(".list-group-item").on('click', function () {
   var templatePath = $(this).attr("data-link");
   // templatePath = "01.html";
+  var pageTitle = $(this).text();
+  $("#page-name").text(pageTitle);
 
-  $.get("./assets/templates/" +templatePath, function(pageHTML) {
+  $.get("./assets/templates/" + templatePath, function(pageHTML) {
     console.log(pageHTML);
     $(".content-body").html(pageHTML);
   })
