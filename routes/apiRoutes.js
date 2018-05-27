91216c29261b3960efd3673c25a600aba8300ea7
Daniel Foster <danieljfoster87@gmail.com>
2018-05-27 15:25:16 -0400

    added more info to ajax section

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,4 @@
 
-// var path = require("path");
-
-// module.exports = function(app) {
-//   app.get("/api/loginCheck", function(req, res) {
-//     if (req.user) {
-//       res.json(true);
-//     } else {
-//       res.json(false);
-//     }
-//   })
-// };
-
 var path = require("path");
 
 // import formidable
